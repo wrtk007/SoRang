af225f038532f39c58a970f6676649e9073d0998
hansu-kim <31onenumber@gmail.com>
2020-11-17 12:45:51 +0900

    map 합치기

--- DDL/CREATE_TABLE_Origin.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE IF NOT EXISTS `team15`.`origin_box` (`no` INT(5) NOT NULL AUTO_INCREMENT, `origin` varchar(30), primary key(`no`)) ;
--- DDL/CREATE_TABLE_QnA.sql
+++
@@ -1,7 +0,0 @@
- create table `team15`.`qna` (
-    number int unsigned not null primary key auto_increment,
-    title varchar(150) not null,
-    content text not null,
-    id varchar(20) not null,
-    date datetime not null,
-   );
