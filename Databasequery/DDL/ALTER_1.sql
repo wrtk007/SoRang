0efbb8f83da83126fed2ab0d14182f1fbef3a403
TK <wrtk007@gmail.com>
2020-11-18 00:04:03 +0900

    ALTER TABLE_1

---
+++ Databasequery/DDL/ALTER_1.sql
@@ -0,0 +1,10 @@
+ALTER TABLE `Beer` ADD `table_no` INT(5) NOT NULL;
+ALTER TABLE `Liquor` ADD `table_no` INT(5) NOT NULL;
+ALTER TABLE `Nonalcohol` ADD `table_no` INT(5) NOT NULL;
+ALTER TABLE `Wine` ADD `table_no` INT(5) NOT NULL;
+ALTER TABLE `Cocktail` ADD `table_no` INT(5) NOT NULL;
+ALTER TABLE `Etc` ADD `table_no` INT(5) NOT NULL;
+ALTER TABLE `Makgeolli` ADD `table_no` INT(5) NOT NULL;
+ALTER TABLE `Soju` ADD `table_no` INT(5) NOT NULL;
+
+
